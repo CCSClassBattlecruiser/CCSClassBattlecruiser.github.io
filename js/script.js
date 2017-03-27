8c22cc0c5ff573ff69fe1a0972cfe412fb4ce0cb
CCSClassBattlecruiser <623161603@qq.com>
2017-03-27 16:28:33 +0800

    change css attribute to link

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function daychange()
+    {
+    var time = new Date().getHours;
+    element = document.getElementById('planeptune')
+    if(6<time<20)
+    {
+            element.src="/backgroudimage/1-day.jpg";
+           }
+        else
+           {
+            element.src="/backgroudimage/1-night.jpg";
+           }
+    }
