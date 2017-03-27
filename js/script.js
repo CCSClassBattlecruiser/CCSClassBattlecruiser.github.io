fd3317c0994bfeea28718f8f5fb375047b91e925
CCSClassBattlecruiser <623161603@qq.com>
2017-03-28 01:13:54 +0800

    js change

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function daychange()
     {
     var time = new Date().getHours;
     element = document.getElementById('planeptune')
-    if(6<time<20)
+    if(6<time&&time<20)
     {
             element.src="backgroudimage/1-day.jpg";
            }
