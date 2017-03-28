ce590cce52e6ea44b9bd657feb1541b7f8a8ef21
CCSClassBattlecruiser <623161603@qq.com>
2017-03-28 13:46:20 +0800

    Add MMD viewer

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 function daychange()
     {
-    var time = new Date().getHours;
-    element = document.getElementById('planeptune')
-    if(6<time&&time<20)
+    var date =new Date();
+    var time = date.getHours;
+    element = document.getElementById('planeptune');
+     if(6<time&&time<20)
+    { 
+        element.src="backgroudimage/1-day.jpg";
+    }
+    else
     {
-            element.src="backgroudimage/1-day.jpg";
-           }
-        else
-           {
-            element.src="backgroudimage/1-night.jpg";
-           }
+    element.src="backgroudimage/1-night.jpg";
+    }
     }
